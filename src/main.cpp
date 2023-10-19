d668fde03066cf46fccf4d8e5bcd40027595b308
Đorđe Cupać <cupacdjordje@gmail.com>
2023-10-19 14:45:01 +0200

    Set up stvec

--- src/main.cpp
+++ src/main.cpp
@@ -1,5 +1,6 @@
 #include "../lib/console.h"
 
+extern "C" void trap();
 
 void interrupt_handler()
 {
@@ -9,6 +10,8 @@ void interrupt_handler()
 
 int main()
 {
+    asm volatile("csrw stvec, %0" ::"r"(trap));
+
     int a = 0;
     a++;
     __putc('v');
