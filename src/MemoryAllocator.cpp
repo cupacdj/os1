d850ccb371558ffdc7e4295da97173aeadfc3b28
cupacdj <cupacdjordje@gmail.com>
2024-06-22 22:46:53 +0200

    mem_alloc

--- src/MemoryAllocator.cpp
+++ src/MemoryAllocator.cpp
@@ -15,5 +15,45 @@ MemoryAllocator* MemoryAllocator::getInstance() {
 }
 
 void* MemoryAllocator::mem_alloc(size_t size) {
-    
+    if(size <= 0) return nullptr;
+
+    size_t numOfBlocks = (size + MEM_BLOCK_SIZE - 1) / MEM_BLOCK_SIZE + 1;
+    size_t numOfBlocksBytes = numOfBlocks * MEM_BLOCK_SIZE;
+
+    Block* curr = head;
+    if(curr == nullptr) {
+        return nullptr;
+    }
+
+    while(curr != nullptr ){
+        if(curr->size >= numOfBlocksBytes){
+            Block* newFreeBlock = (Block*)((char*)curr + numOfBlocksBytes);
+            newFreeBlock->size = curr->size - numOfBlocksBytes;
+            newFreeBlock->next = curr->next;
+            newFreeBlock->prev = curr;
+            if(curr->next != nullptr){
+                curr->next->prev = newFreeBlock;
+            }
+            curr->next = newFreeBlock;
+            curr->size = numOfBlocksBytes;
+
+            if(curr->prev == nullptr){
+                head = curr->next;
+            } else {
+                curr->prev->next = curr->next;
+            }
+            if(curr->next != nullptr){
+                curr->next->prev = curr->prev;
+            }
+
+            return (void*)((char*)curr + MEM_BLOCK_SIZE);
+        }
+        curr = curr->next;
+    }
+    return nullptr;
 }
+
+int MemoryAllocator::mem_free(void* ptr) {
+    if(ptr == nullptr) return -1;
+
+}   
